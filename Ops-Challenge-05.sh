39032462025a587fc4b734cde2d4722b669fe715
EthanBrock1 <ethancbrock13@gmail.com>
2023-02-11 19:00:50 +0000

    OpsCh5 finished

--- Ops-Challenge-05.sh
+++ Ops-Challenge-05.sh
@@ -1,1 +1,29 @@
+#!/bin/bash
 
+# Script: Ops 201 Class 05 Ops Challenge Solution
+# Author:Ethan Brock
+# Date of latest revision:10FEB2023
+# Purpose: Kill PID
+
+# Main
+
+ps aux
+
+echo "Would you like to kill a PID number? [yes/no]"
+read kill_option
+
+while [[ $kill_option == "yes" ]]
+do
+    ps aux
+    echo "Please enter PID number to kill."
+    read input_var
+    echo "killing PID $input_var."
+    kill -9 $input_var
+    echo "PID $input_var killed."
+    echo "Would you like to kill another?"
+    read kill_option
+done
+
+echo "Goodbye"
+
+# End
