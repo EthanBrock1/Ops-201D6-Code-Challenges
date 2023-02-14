1d795eac97029d371ff1376287a42b5206490cae
EthanBrock1 <ethancbrock13@gmail.com>
2023-02-14 03:32:19 +0000

    Upload 06 Challenge

---
+++ Ops-Challenge-06.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# Script: Ops 201 Class 06 Ops Challenge Solution
+# Author:Ethan Brock
+# Date of latest revision:13FEB2023
+# Purpose: Search for File/Directory and creat if not found
+
+# Main
+
+types=("File" "Directory")
+
+echo "Would you like to search for a File or Directory"
+read type
+    while [[ "${types[@]}" =~ "$type"  ]]; do
+        if [[ $type == "File" ]]; then
+            echo "What File would you like to search for?"
+            read filename
+                if [[ -f "$filename" ]]; then
+                    echo "$filename exists."
+                else 
+                    echo "$filename does not exist, creating $filename now."
+                    touch $filename
+                    break
+                fi
+        elif [[ $type == "Directory" ]]; then
+            echo "What Directory would you like to seach for?"
+            read dirname
+                if [[ -d "$dirname" ]]; then
+                    echo "$dirname exists."
+                else
+                    echo "Directory does not exist, creating $dirname now"
+                    mkdir $dirname
+                    break
+                fi
+        fi
+    done
