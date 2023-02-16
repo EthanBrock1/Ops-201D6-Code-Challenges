c251dc0c2a14f2096ba15bbbf5a63c75d7933247
Ethan Brock <123278460+EthanBrock1@users.noreply.github.com>
2023-02-15 23:44:48 -0500

    fixed

--- Ops-Challenge-08.bat
+++ Ops-Challenge-08.bat
@@ -1,1 +1,9 @@
-robocopy c:\jorge\workfiles E:\jorgebackup\workfilesbackup /E 
+# Script: Ops 201 Class 08 Ops Challenge Solution
+# Author: Ethan Brock
+# Date of latest revision: 15FEB2023
+# Purpose: Backup Files
+
+# Main
+robocopy C:\Users\j.thompson\Desktop\"My Project Files" C:\Users\j.thompson\Backups /s
+
+# End
